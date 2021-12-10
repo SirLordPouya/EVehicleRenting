d2e2315495e55e8396b9f2b40f366acc03f5fe12
Pouya Heydari <pouyaheydary@gmail.com>
2021-12-10 20:31:27 +0100

    Added MapsFragment

---
+++ app/src/main/java/com/pouyaheydari/android/evehiclerenting/presentation/map/MapViewModel.kt
@@ -0,0 +1,9 @@
+package com.pouyaheydari.android.evehiclerenting.presentation.map
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MapViewModel @Inject constructor() : ViewModel() {
+}
---
+++ app/src/main/java/com/pouyaheydari/android/evehiclerenting/presentation/map/MapsFragment.kt
@@ -0,0 +1,42 @@
+package com.pouyaheydari.android.evehiclerenting.presentation.map
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.pouyaheydari.android.evehiclerenting.R
+import com.pouyaheydari.android.evehiclerenting.databinding.FragmentMapsBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MapsFragment : Fragment() {
+
+    private lateinit var binding: FragmentMapsBinding
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMapsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+}
