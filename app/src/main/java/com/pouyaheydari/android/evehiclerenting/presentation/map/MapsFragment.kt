d0f3fb2f398aeea589f2b5bc5cf2cb204766091d
Pouya Heydari <pouyaheydary@gmail.com>
2021-12-10 20:58:45 +0100

    Changed card markers

--- app/src/main/java/com/pouyaheydari/android/evehiclerenting/presentation/map/MapsFragment.kt
+++ app/src/main/java/com/pouyaheydari/android/evehiclerenting/presentation/map/MapsFragment.kt
@@ -1,14 +1,19 @@
 package com.pouyaheydari.android.evehiclerenting.presentation.map
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.DrawableRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.pouyaheydari.android.evehiclerenting.R
@@ -21,6 +26,13 @@ class MapsFragment : Fragment() {
     private lateinit var binding: FragmentMapsBinding
     private val viewModel: MapViewModel by viewModels()
     private var map: GoogleMap? = null
+    private val carBitmap by lazy { bitmapDescriptor(R.drawable.ic_car_marker) }
+
+    private fun bitmapDescriptor(@DrawableRes icon: Int): BitmapDescriptor {
+        val b = BitmapFactory.decodeResource(resources, icon)
+        val smallMarker = Bitmap.createScaledBitmap(b, 100, 100, false)
+        return BitmapDescriptorFactory.fromBitmap(smallMarker)
+    }
 
     private val callback = OnMapReadyCallback { googleMap ->
         map = googleMap
@@ -43,7 +55,9 @@ class MapsFragment : Fragment() {
 
         viewModel.vehiclesLiveData.observe(viewLifecycleOwner) { vehicles ->
             vehicles.forEach {
-                map?.addMarker(MarkerOptions().position(LatLng(it.lat, it.lon)).title(it.title))
+                map?.addMarker(
+                    MarkerOptions().position(LatLng(it.lat, it.lon)).icon(carBitmap).title(it.title)
+                )
             }
         }
     }
