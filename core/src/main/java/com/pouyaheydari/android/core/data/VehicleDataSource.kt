858adb2505e161b0736d49d4f6076190cfabe649
Pouya Heydari <pouyaheydary@gmail.com>
2021-12-10 18:56:26 +0100

    Added Vehicles to domain

--- core/src/main/java/com/pouyaheydari/android/core/data/VehicleDataSource.kt
+++ core/src/main/java/com/pouyaheydari/android/core/data/VehicleDataSource.kt
@@ -1,8 +1,10 @@
 package com.pouyaheydari.android.core.data
 
+import com.pouyaheydari.android.core.domain.Vehicles
+
 interface VehicleDataSource {
 
-    suspend fun getAllVehicles()
+    suspend fun getAllVehicles(): List<Vehicles>
 
     suspend fun getVehicleById(vehicleId: Int)
 
---
+++ core/src/main/java/com/pouyaheydari/android/core/domain/Vehicles.kt
@@ -0,0 +1,22 @@
+package com.pouyaheydari.android.core.domain
+
+data class Vehicles(
+    val address: String,
+    val carId: Int,
+    val city: String,
+    val distance: String,
+    val fuelLevel: Int,
+    val isClean: Boolean,
+    val isDamaged: Boolean,
+    val lat: Double,
+    val licencePlate: String,
+    val locationId: Int,
+    val lon: Double,
+    val pricingParking: String,
+    val pricingTime: String,
+    val reservationState: Int,
+    val title: String,
+    val vehicleStateId: Int,
+    val vehicleTypeId: Int,
+    val zipCode: String
+)
